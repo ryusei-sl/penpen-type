8a433d63443a57f90e385633811cb12d7c4609a0
ryusei-sl <r.hagiya@sophia-links.com>
2022-06-08 19:43:12 +0900

    🐛

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,8 @@ import { FC } from "react";
 export const Header: FC = () => {
   return (
     <div>
-      <header className="flex items-center justify-between px-8 h-14 bg-indigo-200">
-        <Image src="/logo.svg" alt="logo" width={20} height={20} />
+      <header className="flex items-center justify-between px-8 h-[48px] bg-indigo-200">
+        <Image src="/logo.svg" alt="logo" width={32} height={32} />
         <ul>
           <li>
             <Link href="/about">
--- components/Headline.tsx
+++ components/Headline.tsx
@@ -5,7 +5,7 @@ export const Headline: FC = () => {
   return (
     <div className="text-center m-4">
       <h1 className="text-red-600 font-bold text-2xl ">Hello TypeScript</h1>
-      <Image src="/penpen.png" alt="penpen" width={300} height={300} />
+      <Image src="/penpen.png" alt="penpen" width={240} height={240} />
     </div>
   );
 };
